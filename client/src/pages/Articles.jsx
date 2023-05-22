1c0ce4db135013e6be181f8f594145250ce64f93
Carrington <hartis.c99@gmail.com>
2023-05-21 21:15:16 -0400

    api call works

--- client/src/pages/Articles.jsx
+++ client/src/pages/Articles.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export const Articles = () => {
-  return (
-    <div>Articles</div>
-  )
-}
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchAllPosts = async () => {
+      try {
+        const response = await axios.get("https://www.mmobomb.com/api1/latestnews");
+        console.log(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchAllPosts();
+  }, []);
+
+  // return (
+  //   <ul>
+  //     {posts.map((post) => (
+  //       <li key={post.id}>{post.title}</li>
+  //     ))}
+  //   </ul>
+  // );
+};
