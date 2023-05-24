5dbd54fdccddb442bbe7df85df948958866f2b3d
Carrington <hartis.c99@gmail.com>
2023-05-24 13:46:14 -0400

    parse html elements

--- client/src/pages/Article.jsx
+++ client/src/pages/Article.jsx
@@ -6,11 +6,23 @@ export const Article = (props) => {
   const propsData = location.state;
   console.log(props);
   console.log(propsData);
+
+  const htmlString = propsData.article_content;
+  const parser = new DOMParser();
+  // Use DOMParser to parse HTML string
+  const doc = parser.parseFromString(htmlString, "text/html");
+  // Obtain paragraph elements
+  const paragraphElements = doc.getElementsByTagName("p");
+  // Convert paragraphElements to an array
+  const paragraphArray = Array.from(paragraphElements);
+
   return (
     <>
       <h1>{propsData.title}</h1>
       <img src={propsData.thumbnail} alt="Game Thumbnail" />
-      <div>{propsData.short_description}</div>
+      {paragraphArray.map((paragraph) => (
+        <>{paragraph.innerHTML}</>
+      ))}
     </>
   );
 };
