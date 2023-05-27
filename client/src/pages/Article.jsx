441cf36c164cc994d76a5c236e169ec8e8caa3bf
Carrington <hartis.c99@gmail.com>
2023-05-27 13:38:29 -0400

    sanitize data

--- client/src/pages/Article.jsx
+++ client/src/pages/Article.jsx
@@ -1,28 +1,20 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
+import DOMPurify from 'dompurify'
 
-export const Article = (props) => {
+export const Article = () => {
   const location = useLocation();
   const propsData = location.state;
-  console.log(props);
-  console.log(propsData);
 
-  const htmlString = propsData.article_content;
-  const parser = new DOMParser();
-  // Use DOMParser to parse HTML string
-  const doc = parser.parseFromString(htmlString, "text/html");
-  // Obtain paragraph elements
-  const paragraphElements = doc.getElementsByTagName("p");
-  // Convert paragraphElements to an array
-  const paragraphArray = Array.from(paragraphElements);
+  // DOMPurify sanitizes HTML and prevents XSS attacks
+  const sanitizedData = () => ({
+    __html: DOMPurify.sanitize(propsData.article_content)
+  })
 
   return (
     <>
       <h1>{propsData.title}</h1>
-      <img src={propsData.thumbnail} alt="Game Thumbnail" />
-      {paragraphArray.map((paragraph) => (
-        <>{paragraph.innerHTML}</>
-      ))}
+      <div dangerouslySetInnerHTML={sanitizedData()} />
     </>
   );
 };
--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -11,7 +11,6 @@ export const Feed = () => {
       try {
         const response = await axios.get("https://www.mmobomb.com/api1/latestnews");
         setArticles(response.data);
-        console.log(response.data);
       } catch (err) {
         console.log(err);
       }
