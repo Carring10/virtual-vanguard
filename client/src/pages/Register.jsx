0449d0ee5e4f616097f3a821a308808ec870d6f4
Carrington <hartis.c99@gmail.com>
2023-05-29 12:46:18 -0400

    register user in db from frontend

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState, useEffect } from "react";
+import axios from 'axios';
 
 export const Register = () => {
   const [input, setInput] = useState({
@@ -13,13 +14,23 @@ export const Register = () => {
 
   console.log(input)
 
+  const handleClick = async (event) => {
+    event.preventDefault();
+
+    try {
+      await axios.post('http://localhost:8800/auth/register', input)
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <div>
       <h1>Register</h1>
       <form>
         <input type='text' placeholder='Username' name='username' onChange={handleChange} />
         <input type='text' placeholder='Password' name='password' onChange={handleChange} />
-        <button>Register</button>
+        <button onClick={handleClick}>Register</button>
       </form>
     </div>
   )
