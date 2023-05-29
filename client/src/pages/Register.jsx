393c752d4b3e53b5dfd5c6823398eb8dd8939cff
Carrington <hartis.c99@gmail.com>
2023-05-29 12:36:41 -0400

    useState for register inputs

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
+import { useState, useEffect } from "react";
 
 export const Register = () => {
+  const [input, setInput] = useState({
+    username: '',
+    password: ''
+  });
+
+  const handleChange = (event) => {
+    setInput((prev) => ({ ...prev, [event.target.name]: event.target.value }));
+  }
+
+  console.log(input)
+
   return (
     <div>
       <h1>Register</h1>
       <form>
-        <input type='text' placeholder='Username' />
-        <input type='text' placeholder='Password' />
+        <input type='text' placeholder='Username' name='username' onChange={handleChange} />
+        <input type='text' placeholder='Password' name='password' onChange={handleChange} />
         <button>Register</button>
       </form>
     </div>
