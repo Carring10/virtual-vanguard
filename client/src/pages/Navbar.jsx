7cafdbd055789b2cb19bf54d0598a53aec1c5387
Carrington <hartis.c99@gmail.com>
2023-06-04 16:26:00 -0400

    check if user is logged in

--- client/src/pages/Navbar.jsx
+++ client/src/pages/Navbar.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const user = JSON.parse(localStorage.getItem("user"));
-
-export const Navbar = () => {
-  return (
-    <>
+function isLoggedIn() {
+  const user = JSON.parse(localStorage.getItem("user"));
+  if (user) {
+    return <Link>Logout</Link>;
+  } else {
+    return (
       <div>
         <Link to="/login">Sign In</Link>
         <p>{user && user.username}</p>
       </div>
-    </>
-  );
+    );
+  }
+}
+
+export const Navbar = () => {
+  return <>{isLoggedIn()}</>;
 };
