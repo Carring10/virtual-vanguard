f903fcc1b1f2b09dba2f91b86cb95fe1a972fb47
Carrington <hartis.c99@gmail.com>
2023-06-17 13:31:45 -0400

    display articles comments

--- client/src/pages/Comments.jsx
+++ client/src/pages/Comments.jsx
@@ -1,10 +1,11 @@
-import { useContext, useState } from "react";
-import { AuthContext } from "../../src/context/authContext";
-import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { useContext, useState } from 'react';
+import { AuthContext } from '../../src/context/authContext';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import axios from 'axios';
+import moment from 'moment/moment';
 
 export const Comments = ({ articleId }) => {
-  const [content, setContent] = useState("");
+  const [content, setContent] = useState('');
   const { currentUser } = useContext(AuthContext);
   const userId = currentUser.id;
 
@@ -12,19 +13,19 @@ export const Comments = ({ articleId }) => {
 
   const { isLoading, error, data } = useQuery(['comments'], () => 
     axios.get(`http://localhost:8800/comments/${articleId}`).then((res) => {
-      console.log(res.data);
-      return res.data;
+      const data = res.data.comments;
+      return data;
     })
-  )
+  );
 
   // Mutation used to make changes to the server, provide data as 'newComment'
   const mutation = useMutation(
     (newComment) => {
-      return axios.post("http://localhost:8800/comments", newComment);
+      return axios.post('http://localhost:8800/comments', newComment);
     },
     {
       onSuccess: () => {
-        queryClient.invalidateQueries(["comments"]);
+        queryClient.invalidateQueries(['comments']);
       },
     }
   );
@@ -32,21 +33,30 @@ export const Comments = ({ articleId }) => {
   const handleClick = async (event) => {
     event.preventDefault();
     mutation.mutate({ userId, content, articleId });
-    setContent("");
+    setContent('');
   };
-
+  console.log(data)
   return (
-    <div className="comments">
-      <div className="write">
+    <div className='comments'>
+      <div className='write'>
         <p>{currentUser.username}</p>
         <input
-          type="text"
-          placeholder="Write a comment"
+          type='text'
+          placeholder='Write a comment'
           value={content}
           onChange={(event) => setContent(event.target.value)}
         />
         <button onClick={handleClick}>Send</button>
       </div>
+      {data && data.map((comment) => (
+        <div className='comment' key={comment.index}>
+          <div className='user-info'>
+            <h2>{comment.username}</h2>
+            <p>{comment.content}</p>
+          </div>
+          <span className='date'>{moment(comment.createdAt).fromNow()}</span>
+        </div>
+      ))}
     </div>
   );
 };
