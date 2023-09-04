1278abf38d43e82e0a0ec213180d8b431eee882e
Carrington <hartis.c99@gmail.com>
2023-09-04 14:10:08 -0400

    search parameter array for api call

--- client/src/pages/Discover.jsx
+++ client/src/pages/Discover.jsx
@@ -9,7 +9,7 @@ export const Discover = () => {
     const fetchAllGames = async () => {
       try {
         const response = await axios.get(
-          "https://www.mmobomb.com/api1/filter?tag=3d.mmorpg.fantasy.pvp&platform=pc"
+          "https://www.mmobomb.com/api1/filter?tag=mmorpg.fantasy.open-world.pvp"
         );
         setGames(response.data);
       } catch (err) {
@@ -20,56 +20,66 @@ export const Discover = () => {
   }, []);
 
   console.log(games);
+  const searchParam = [];
+
+  const getTag = (event) => {
+    const tags = event.target.textContent;
+
+    searchParam.push(tags);
+
+    console.log(searchParam)
+  }
+
   return (
     <>
       <h1>Discover Games</h1>
       <div className="search-params-container">
         <p>Genre</p>
         <div className="genre">
-          <button>mmorpg</button>
-          <button>shooter</button>
-          <button>strategy</button>
-          <button>moba</button>
-          <button>racing</button>
-          <button>sports</button>
-          <button>survival</button>
-          <button>mmo</button>
-          <button>mmofps</button>
-          <button>mmotps</button>
-          <button>mmorts</button>
-          <button>horror</button>
-          <button>racing</button>
-          <button>tower-defense</button>
-          <button>card</button>
+          <button onClick={getTag}>mmorpg</button>
+          <button onClick={getTag}>shooter</button>
+          <button onClick={getTag}>strategy</button>
+          <button onClick={getTag}>moba</button>
+          <button onClick={getTag}>racing</button>
+          <button onClick={getTag}>sports</button>
+          <button onClick={getTag}>survival</button>
+          <button onClick={getTag}>mmo</button>
+          <button onClick={getTag}>mmofps</button>
+          <button onClick={getTag}>mmotps</button>
+          <button onClick={getTag}>mmorts</button>
+          <button onClick={getTag}>horror</button>
+          <button onClick={getTag}>racing</button>
+          <button onClick={getTag}>tower-defense</button>
+          <button onClick={getTag}>card</button>
         </div>
 
         <div className="style">
           <p>Style</p>
-          <button>sandbox</button>
-          <button>open-world</button>
-          <button>pvp</button>
-          <button>pve</button>
-          <button>pixel</button>
-          <button>voxel</button>
-          <button>turn-based</button>
-          <button>top-down</button>
-          <button>side-scroller</button>
+          <button onClick={getTag}>sandbox</button>
+          <button onClick={getTag}>open-world</button>
+          <button onClick={getTag}>pvp</button>
+          <button onClick={getTag}>pve</button>
+          <button onClick={getTag}>pixel</button>
+          <button onClick={getTag}>voxel</button>
+          <button onClick={getTag}>turn-based</button>
+          <button onClick={getTag}>top-down</button>
+          <button onClick={getTag}>side-scroller</button>
         </div>
 
         <div className="theme">
           <p>Theme</p>
-          <button>zombie</button>
-          <button>space</button>
-          <button>sailing</button>
-          <button>superhero</button>
-          <button>anime</button>
-          <button>fantasy</button>
-          <button>sci-fi</button>
-          <button>fighting</button>
-          <button>action</button>
-          <button>military</button>
-          <button>martial-arts</button>
-          <button>flight</button>
+          <button onClick={getTag}>zombie</button>
+          <button onClick={getTag}>space</button>
+          <button onClick={getTag}>sailing</button>
+          <button onClick={getTag}>superhero</button>
+          <button onClick={getTag}>anime</button>
+          <button onClick={getTag}>fantasy</button>
+          <button onClick={getTag}>sci-fi</button>
+          <button onClick={getTag}>fighting</button>
+          <button onClick={getTag}>action</button>
+          <button onClick={getTag}>military</button>
+          <button onClick={getTag}>martial-arts</button>
+          <button onClick={getTag}>flight</button>
         </div>
       </div>
     </>
