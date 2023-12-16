3a64fcd959549549ea7afc3afab473f217f742f2
Carrington <hartis.c99@gmail.com>
2023-12-16 14:48:09 -0500

    giveaways page

--- client/src/pages/Giveaways/Giveaways.jsx
+++ client/src/pages/Giveaways/Giveaways.jsx
@@ -3,17 +3,16 @@ import { useLocation } from "react-router-dom";
 import { Navbar } from "../Navbar/Navbar";
 import { Link } from "react-router-dom";
 import axios from "axios";
-// import "./game.css";
+import "./giveaways.css";
 
 export const Giveaways = () => {
   const [giveAways, setGiveAways] = useState([]);
 
   useEffect(() => {
-
     const fetchGiveAways = async () => {
       try {
         const response = await axios.get("https://www.mmobomb.com/api1/giveaways");
-        console.log(response.data)
+        console.log(response.data);
         setGiveAways(response.data);
       } catch (err) {
         console.log(err);
@@ -21,12 +20,26 @@ export const Giveaways = () => {
     };
     fetchGiveAways();
   }, []);
-  console.log(giveAways)
+  console.log(giveAways);
 
   return (
     <>
       <Navbar />
-      <h1>Giveaways</h1>
+      <div className="giveaways-container">
+        <h1>Giveaways</h1>
+        {giveAways.map((giveAway, index) => (
+          <div className="give-aways-container">
+            <div>
+              <p>{giveAway.title}</p>
+              <img src={giveAway.thumbnail} alt="Giveaway Thumbnail" />
+              <p>{giveAway.short_description}</p>
+            </div>
+            <div>
+              <p>ONLY {giveAway.keys_left} OF KEYS LEFT!</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
