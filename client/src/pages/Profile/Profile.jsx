310144958b5da08992183b906c01241d854e97a9
Carrington <hartis.c99@gmail.com>
2023-12-30 14:59:59 -0500

    profile page

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,10 +1,26 @@
+import { useContext, useState } from "react";
+import { AuthContext } from "../../context/authContext";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
 import { Navbar } from "../Navbar/Navbar";
+import defaultPic from "../../images/default-pic.jpg";
+import './profile.css';
 
 export const Profile = () => {
-  console.log("PROFILE");
+  const { currentUser } = useContext(AuthContext);
+  console.log(currentUser);
   return (
     <>
       <Navbar />
+      <div className="profile-container">
+        <div className="profile-pic-container">
+          <img src={defaultPic} alt="Default" className="profile-pic" />
+        </div>
+        <div className="user-info-container">
+          <p>Username</p>
+          <p>{currentUser.username}</p>
+        </div>
+      </div>
     </>
   );
 };
