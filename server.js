6960d50591979b01310375ca5df996873acb9306
Carrington <hartis.c99@gmail.com>
2024-05-19 12:04:36 -0400

    changed wildcard

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ const corsMiddleware = (req, res, next) => {
 
 const applyCorsHeaders = res => {
   res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Origin', '/')
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS, PATCH, DELETE, POST, PUT')
   res.setHeader(
     'Access-Control-Allow-Headers',
