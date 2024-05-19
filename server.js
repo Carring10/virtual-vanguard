809be75894bd7ddc8e4aa9cc5e49c082306bc1b8
Carrington <hartis.c99@gmail.com>
2024-05-19 12:27:59 -0400

    changed credentials to include

--- server.js
+++ server.js
@@ -10,43 +10,43 @@ const cors = require('cors');
 const PORT = process.env.PORT || 8800;
 
 // Middleware
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Credentials", 'true');
-//   next();
-// })
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", 'true');
+  next();
+})
 
 app.use(express.json());
 
-// app.use(cors({
-//   origin: "https://virtual-vanguard.netlify.app",
-//   credentials: 'true',
-//   sameSite: 'None'
-// }));
+app.use(cors({
+  origin: "https://virtual-vanguard.netlify.app",
+  credentials: 'include',
+  // sameSite: 'None'
+}));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-const corsMiddleware = (req, res, next) => {
-  res = applyCorsHeaders(res);
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  next()
-}
+// const corsMiddleware = (req, res, next) => {
+//   res = applyCorsHeaders(res);
+//   if (req.method === 'OPTIONS') {
+//     res.status(200).end()
+//     return
+//   }
+//   next()
+// }
 
-const applyCorsHeaders = res => {
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Origin', 'https://virtual-vanguard-mmo-f84f119b0dd9.herokuapp.com')
-  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS, PATCH, DELETE, POST, PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  return res;
-}
+// const applyCorsHeaders = res => {
+//   res.setHeader('Access-Control-Allow-Credentials', 'true');
+//   res.setHeader('Access-Control-Allow-Origin', 'https://virtual-vanguard-mmo-f84f119b0dd9.herokuapp.com')
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS, PATCH, DELETE, POST, PUT')
+//   res.setHeader(
+//     'Access-Control-Allow-Headers',
+//     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+//   )
+//   return res;
+// }
 
-app.use(corsMiddleware);
+// app.use(corsMiddleware);
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
