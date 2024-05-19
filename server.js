8e38f77af3648001dce758539bbf38257248cf03
Carrington <hartis.c99@gmail.com>
2024-05-19 14:36:57 -0400

    configure cookies

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cors({
   origin: "https://virtual-vanguard.netlify.app",
   credentials: true,
-  // sameSite: 'None'
+  sameSite: 'None'
 }));
 
 app.use(express.urlencoded({ extended: false }));
