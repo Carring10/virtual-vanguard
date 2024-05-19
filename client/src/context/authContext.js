db759887edff11da68e06cc5443d866e06ba92d4
Carrington <hartis.c99@gmail.com>
2024-05-19 12:22:38 -0400

    cors

--- client/src/context/authContext.js
+++ client/src/context/authContext.js
@@ -14,7 +14,7 @@ export const AuthContextProvider = ({ children }) => {
   const login = async (input) => {
     const res = await axios.post('https://virtual-vanguard-mmo-f84f119b0dd9.herokuapp.com/auth/login', input, {
       withCredentials: 'true',
-      sameSite: "none"
+      // sameSite: "none"
     });
     setCurrentUser(res.data);
   }
