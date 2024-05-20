979187961788e1cb528bc55fe9238c2dac8a947f
Carrington <hartis.c99@gmail.com>
2024-05-20 15:35:55 -0400

    connection.js updated

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,23 @@
 require("dotenv").config();
 const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD
+let db;
+
+if (process.env.JAWSDB_URL) {
+  db = mysql.createConnection(process.env.JAWSDB_URL);
+  console.log('Connected to JawsDB database.');
+} else {
+  db = mysql.createConnection({
+    host: 'localhost',
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+  });
+  console.log('Connected to the local database.');
+}
+
+db.connect((err) => {
+  if (err) throw err;
 });
 
-module.exports = pool.promise();
+module.exports = db.promise();
