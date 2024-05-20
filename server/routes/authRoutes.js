302ee41b647a3a3db42319063d208f45c305030c
Carrington <hartis.c99@gmail.com>
2024-05-20 13:16:24 -0400

    changed login to get

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,7 +3,7 @@ const authControllers = require('../controllers/authControllers');
 const router = express.Router();
 
 router.route('/auth/register').post(authControllers.register);
-router.route('/auth/login').post(authControllers.login);
+router.route('/auth/login').get(authControllers.login);
 router.route('/auth/logout').delete(authControllers.logout);
 
 module.exports = router;
