76705c6fbdf42031028b16044069f799afa86130
Carrington <hartis.c99@gmail.com>
2024-05-26 11:47:25 -0400

    default profile pic for new users

--- client/src/context/authContext.js
+++ client/src/context/authContext.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import {createContext, useEffect, useState } from "react";
+import defaultPic from "../upload/default-pic.jpg";
 
 export const AuthContext = createContext();
 
